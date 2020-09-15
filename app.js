11c4dda0cb7196e5894bf0613bce1ac0549ceb5d
joecascioli <joeacascioli@gmail.com>
2020-09-15 14:23:47 -0400

    added basic pages and app serves them instead of text

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 
 const app = express();
+const html_path = __dirname+"/html";
+
+app.get("/", function (request, response) {
+    response.sendFile(html_path + "/index.html");
+})
+
+app.get("/contact", function (request, response) {
+    response.sendFile(html_path + "/contact.html");
+})
+
+app.get("/about", function (request, response) {
+    response.sendFile(html_path + "/about.html");
+})
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
+
